c93256af5b4bd8123cc3eb7830de245ea3b173a2
arklem <jrsy.tdh@yandex.ru>
2024-02-26 01:08:10 +0300

    update settings.py

--- app/settings.py
+++ app/settings.py
@@ -56,6 +56,10 @@ CORS_ALLOWED_ORIGINS = [
     "https://domainparser-k7twijfn.b4a.run"
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://domainparser-k7twijfn.b4a.run"
+]
+
 ROOT_URLCONF = 'app.urls'
 
 TEMPLATES = [
