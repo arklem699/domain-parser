1b1bc9a6b34cd90ba36c7d23d406bc424abc5fe9
arklem <jrsy.tdh@yandex.ru>
2024-02-25 23:53:15 +0300

    update settings.py

--- app/settings.py
+++ app/settings.py
@@ -37,17 +37,23 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'domain_parser',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://domainparser-k7twijfn.b4a.run"
 ]
 
 ROOT_URLCONF = 'app.urls'
